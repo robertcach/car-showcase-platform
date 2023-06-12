d75abe964b5094ba3159a9aa5cb48884ae4df2f7
Robert Cabrera <cabrera.rch@gmail.com>
2023-06-12 17:42:05 +0200

    feat: Create a basic hook to get cars from API and it's interface

---
+++ src/hooks/getCars.ts
@@ -0,0 +1,12 @@
+export function useGetCars(brand: string) {
+  const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=${brand}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_CARS_API_KEY || "",
+      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+
+  return fetch(url, options).then((res) => res.json());
+}
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,3 +2,18 @@ export interface CarBrand {
   id: number;
   name: string;
 }
+
+export interface Car {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
