b575665128dbf200a24ab265e277853c60553bec
Robert Cabrera <cabrera.rch@gmail.com>
2023-06-14 18:56:40 +0200

    feat: Add general image to Card component

--- src/components/car-card.tsx
+++ src/components/car-card.tsx
@@ -1,4 +1,5 @@
 import { Car } from "@/interfaces";
+import Image from "next/image";
 
 interface CarCardProps {
   car: Car;
@@ -7,6 +8,13 @@ interface CarCardProps {
 export default function CarCard({ car }: CarCardProps) {
   return (
     <div>
+      <Image
+        src={"/hero.png"}
+        alt="hero car"
+        width={500}
+        height={500}
+        className="w-auto h-auto"
+      />
       <p>{car.model}</p>
     </div>
   );
