2a55e485b942640aed40fe09b5a145c9ed1fdecd
Robert Cabrera <cabrera.rch@gmail.com>
2023-06-14 18:55:01 +0200

    fix: Update Home page to use hook to get cars from selected brand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,13 @@ import Hero from "@/components/hero";
 import SearchBar from "@/components/search-bar";
 import { CARS_BRANDS } from "@/constants";
 import { useGetCars } from "@/hooks/getCars";
-import { Car, CarBrand } from "@/interfaces";
+
+import { CarBrand } from "@/interfaces";
 import { useState } from "react";
 
-export default async function Home() {
+export default function Home() {
   const [selectedBrand, setSelectedBrand] = useState<CarBrand>(CARS_BRANDS[0]);
-  const allCars: Car[] = await useGetCars(selectedBrand.name.toLowerCase());
-
-  console.log(selectedBrand);
+  const { cars, isLoading } = useGetCars(selectedBrand.name);
 
   return (
     <main>
@@ -21,9 +20,17 @@ export default async function Home() {
         selectedBrand={selectedBrand}
         setSelectedBrand={setSelectedBrand}
       />
-      {allCars.map((car) => (
-        <CarCard car={car} />
-      ))}
+      {isLoading ? (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <div className="flex gap-10 py-10 bg-white">
+          {cars?.map((car, index) => (
+            <CarCard car={car} key={index} />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
