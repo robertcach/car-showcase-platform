21d76cce4cc111001d7202332a58ecb443158f61
Robert Cabrera <cabrera.rch@gmail.com>
2023-06-22 17:23:47 +0200

    style(BrandCars): Update styles tow show more data in CarBrand page

--- src/app/brands/[id]/page.tsx
+++ src/app/brands/[id]/page.tsx
@@ -1,17 +1,26 @@
 import { useGetBrand } from "@/hooks/getBrand";
 import { Car } from "@/interfaces";
+import Image from "next/image";
 
 export default async function CarBrand({ params }: any) {
   const { id } = params;
   const brandCars: Car[] = await useGetBrand(id);
 
   return (
-    <div>
-      <ul>
-        {brandCars.map((car) => (
+    <div className="flex flex-wrap gap-20">
+      {brandCars.map((car) => (
+        <article>
+          <Image
+            src={"/hero.png"}
+            alt="hero car"
+            width={200}
+            height={300}
+            className="h-auto"
+          />
           <p>{car.model}</p>
-        ))}
-      </ul>
+          <p>{car.year}</p>
+        </article>
+      ))}
     </div>
   );
 }
