4af5c1145e64a19238a5da3fd770741059fbfae4
Robert Cabrera <cabrera.rch@gmail.com>
2023-06-27 18:17:10 +0200

    feat(CarCard): Update CarCard component to link image to an URL to get car data

--- src/components/car-card.tsx
+++ src/components/car-card.tsx
@@ -1,5 +1,6 @@
 import { Car } from "@/interfaces";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CarCardProps {
   car: Car;
@@ -8,13 +9,18 @@ interface CarCardProps {
 export default function CarCard({ car }: CarCardProps) {
   return (
     <div>
-      <Image
-        src={"/hero.png"}
-        alt="hero car"
-        width={200}
-        height={300}
-        className="h-auto"
-      />
+      <Link
+        href={{ pathname: `/cars/${car.model}`, query: { brand: car.make } }}
+      >
+        <Image
+          src={"/hero.png"}
+          alt="hero car"
+          width={200}
+          height={300}
+          className="h-auto"
+        />
+      </Link>
+
       <p>{car.model}</p>
     </div>
   );
