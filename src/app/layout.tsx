9c2d7f05e050b4b7afc5e5a7712af6ab297efafa
Robert Cabrera <cabrera.rch@gmail.com>
2023-07-01 12:11:05 +0200

    feat: Add context provider to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { FavouritesCarsContextProvider } from "@/contexts/favourites-cars";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        <main>{children}</main>
-      </body>
-    </html>
+    <FavouritesCarsContextProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          <main>{children}</main>
+        </body>
+      </html>
+    </FavouritesCarsContextProvider>
   );
 }
